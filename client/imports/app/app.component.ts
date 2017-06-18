b16c673921ae86ce12d42694f6ff8867b77e7635
Dante Kelly <dante.kelly97@gmail.com>
2017-06-18 04:12:36 -0700

    Added scroll wheel function

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 import template from './app.component.html';
 import style from './app.component.scss';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app',
   template,
   styles: [ style ]
 })
+
 export class AppComponent {
-  constructor() {
+  constructor(private router: Router) {
+    var newnew = ['/home', '/about', '/services', '/portfolio', '/news']
+    var delay = 500; // delay in milliseconds
+
+    $(window).bind('mousewheel DOMMouseScroll', function(event){
+      var newloops = newnew.indexOf(router.url);
+      this.resizeTO = setTimeout(function() {
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            // scroll up
+            if (newloops < newnew.length-1) {
+              router.navigateByUrl(newnew[newloops+1]);
+            }
+        }
+        else {
+            // scroll down
+            if (newloops > 0) {
+              router.navigateByUrl(newnew[newloops-1]);
+            }
+        }
+      }, 250);
+
+
+  });
   }
+
 }
